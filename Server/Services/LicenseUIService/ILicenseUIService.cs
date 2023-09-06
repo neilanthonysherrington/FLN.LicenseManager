f85addd07ce86f8b1178fbf0d5f6403a87819050
barrysherrington <barrysherrington@hotmail.com>
2023-09-06 12:28:00 +0100

    Add filter functionality

--- Server/Services/LicenseUIService/ILicenseUIService.cs
+++ Server/Services/LicenseUIService/ILicenseUIService.cs
@@ -3,5 +3,10 @@
     public interface ILicenseUIService
     {
         Task<ServiceResponse<List<License>>> GetLicensesAsync();
+        //Task<ServiceResponse<License>> GetLicenseAsync(int id);
+        //Task<ServiceResponse<License>> AddLicenseAsync(License license);
+        //Task<ServiceResponse<License>> AddExistingLicenseAsync(License license);
+        //Task<ServiceResponse<License>> UpdateLicenseAsync(License license);
+        //Task<ServiceResponse<License>> DeleteLicenseAsync(int id);
     }
 }
