26875e9f67a89639ac187ae0b97af97f9ab39cc6
neilanthonysherrington <neil.sherrington@frontline-consultancy.co.uk>
2023-10-12 08:51:29 +0100

    Refactor

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -5,9 +5,7 @@ namespace FLN.LicenseManager.Server.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-            
-        }
+        { }
 
         public DbSet<User>? Users { get; set; }
         public DbSet<License>? Licenses { get; set; }
