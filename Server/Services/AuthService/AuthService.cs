be904b932eec3291713b7dc0a8e1c65ca2af269e
neilanthonysherrington <neil.sherrington@frontline-consultancy.co.uk>
2023-10-12 09:00:35 +0100

    Refactor

--- Server/Services/AuthService/AuthService.cs
+++ Server/Services/AuthService/AuthService.cs
@@ -101,13 +101,9 @@ namespace FLN.LicenseManager.Server.Services.AuthService
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
-
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
             var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: creds);
-
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
             return jwt;
         }
     }
